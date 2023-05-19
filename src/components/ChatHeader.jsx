6eca30ae8c7b7cb0d41f1af0215571692964b592
Nastya <asenka0301@gmail.com>
2023-05-19 14:01:56 +0300

    feat: add chat page

---
+++ src/components/ChatHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatHeader = () => (
+  <div className="bg-light mb-4 p-3 shadow-sm small">
+    <p className="m-0">
+      <b>Контакт</b>
+    </p>
+  </div>
+);
+
+export default ChatHeader;
---
+++ src/components/Messages.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Messages = () => (
+  <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+    Messages
+  </div>
+);
+
+export default Messages;
